b15ab6d68df13f87df10a047ab0c5ae2526535f6
Vinicius Lima <vinicius_lima31@hotmail.com>
2023-03-31 15:10:59 -0300

    Comentarios

--- src/main/java/org/gm2/pdv/controller/ProductController.java
+++ src/main/java/org/gm2/pdv/controller/ProductController.java
@@ -16,11 +16,13 @@ public class ProductController {
     @Autowired
     private ProductRepository productRepository;
 
+    // SELECT * FROM
     @GetMapping
     public ResponseEntity getAll() {
         return new ResponseEntity<>(productRepository.findAll(), HttpStatus.OK);
     }
 
+    // INSERT
     @PostMapping
     public ResponseEntity post(@RequestBody Product product) {
         try {
@@ -31,6 +33,7 @@ public class ProductController {
         }
     }
 
+    // UPDATE
     @PutMapping
     public ResponseEntity put(@RequestBody Product product) {
         try {
@@ -41,6 +44,7 @@ public class ProductController {
         }
     }
 
+    // DELETE
     @DeleteMapping
     public ResponseEntity delete(@PathVariable long id) {
         try {
